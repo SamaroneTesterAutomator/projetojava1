4c782fdc9ce8437f91d6504c223859e799ab825d
= <=>
2022-01-09 23:52:07 -0300

    Atualização da classe Pessoa

--- projetojava1/src/curso/java/aulagit/Pessoa.java
+++ projetojava1/src/curso/java/aulagit/Pessoa.java
@@ -2,4 +2,25 @@ package curso.java.aulagit;
 
 public class Pessoa {
 
+	private String nome;
+	
+	private String sobreNome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobreNome() {
+		return sobreNome;
+	}
+
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+	
+	
 }
